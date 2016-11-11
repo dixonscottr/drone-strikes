43aa7fb362d746b2c4dcc333c5b4be5404fe1289
Scott Dixon <email": null>
2016-11-11 12:13:07 -0500

    Add show page

--- app/controllers/dronestrikes.rb
+++ app/controllers/dronestrikes.rb
@@ -2,3 +2,12 @@ get '/dronestrikes' do
   @strikes = Dronestrike.all
   erb :'/dronestrikes/index'
 end
+
+get '/dronestrikes/:dronestrike_id' do
+  @strike = Dronestrike.find_by(id: params[:dronestrike_id])
+  if request.xhr?
+    erb :'/dronestrikes/_add_info', layout: false
+  else
+    erb :'/dronestrikes/show'
+  end
+end
